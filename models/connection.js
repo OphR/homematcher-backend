e33d251470587252af9317c5d2e576f37bd49e00
Clément Boulanger <clement.boulangerpro@gmail.com>
2024-03-03 15:28:36 +0100

    first commit

---
+++ models/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+const connectionString = process.env.CONNECTION_STRING
+mongoose.set("strictQuery", true)
+
+mongoose
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log("Successfully connected to the Homematcher Database 🥳 !"))
+  .catch((errorMessage) => console.error(errorMessage))
