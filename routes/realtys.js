a082595c2fdfe040e83ddf457af04b1afeab2221
hadi77420 <155539749+hadi77420@users.noreply.github.com>
2024-03-10 09:25:51 +0100

    Rajout de la Configuration Cloudinary pour pouvoir enregistrer les images grâce à la route Upload coté backend
    //Configuration de Cloudinary
    cloudinary.config({
      cloud_name: 'YOUR_CLOUD_NAME',
      api_key: 'YOUR_API_KEY',
      api_secret: 'YOUR_API_SECRET'
    });

--- routes/realtys.js
+++ routes/realtys.js
@@ -111,7 +111,12 @@ router.delete('/delete/:realtyId', async (req, res) => {
   }
 });
 
-
+// Configuration de Cloudinary
+cloudinary.config({
+  cloud_name: 'YOUR_CLOUD_NAME',
+  api_key: 'YOUR_API_KEY',
+  api_secret: 'YOUR_API_SECRET'
+});
 router.post('/upload', async (req, res) => {
   try {
     if (!req.files || !req.files.photoFromFront) {
